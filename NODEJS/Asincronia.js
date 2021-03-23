800719df3a8920648e5fd875dea4bc4bc05ea4fd
eddiazPS <edgarhernandodiazmahecha@MacBook-Pro-de-Edgar.local>
2021-03-22 21:42:19 -0500

    Async y otros

--- NODEJS/Asincronia.js
+++ NODEJS/Asincronia.js
@@ -1,8 +1,9 @@
 
 
-//   Asincronia 
+//   
 
 /**
+ * ASINCRONIA 
  * 
  * fundamentalmente  la programacion asicronica    en js se realiza  con devoluciones de llamada (CallBack)
  * 
@@ -39,23 +40,57 @@ function cuadrado (x){
 
 //////////// eliminar el detalle de las dependencias en los clallBacks //////////////////
 // escondemos la dependencia 
-let seconds =30;
+let secondsA =30;
 
-function comenzarCuentaRegresiva (callback){
+function comenzarCuentaRegresivaA (callback){
     setInterval(()=>{   //(comment) lo ejecuta cicilcamente infinito 
-        seconds--;
+        secondsA--;
        callback(); // mostarSegundos();
     },1000);
 }
 
-function mostarSegundos (){
-    console.log(seconds);
+function mostarSegundosA (){
+    console.log(secondsA);
 
 }
 
-//comenzarCuentaRegresiva(mostarSegundos);
+//comenzarCuentaRegresiva(mostarSegundosA);
 //*(coment)eliminamos el conosiiento de mostarr segundos Se elimina la dependencia con callback 
-//comenzarCuentaRegresiva(mostarSegundos);//comenzarCuentaRegresiva();
+//comenzarCuentaRegresiva(mostarSegundosA);//comenzarCuentaRegresivaA();
+
+
+//Eliminar el detalle de las dependencias en los callback
+
+let segundos = 3;
+
+function iniciarCuentaRegresiva() {
+    setInterval(() => {
+        segundos--;
+        if (segundos > 0) {
+            mostrarSegundos();
+        }
+    }, 1000);
+}
+function mostrarSegundos() {
+    console.log(segundos);
+}
+//iniciarCuentaRegresiva();
+
+let segundos2 = 5;
+
+function iniciarCuentaRegresiva2(callback) {
+    setInterval(() => {
+        segundos2--;
+        if (segundos2 > 0) {
+            callback();
+        }
+    }, 1000);
+}
+function mostrarSegundos2() {
+    console.log(segundos2);
+}
+//iniciarCuentaRegresiva2(mostrarSegundos2);
+
 
 
 
@@ -67,7 +102,7 @@ function mostarSegundos (){
  * Errores con los callbacks: manejo de errores  
  *  
  */
-// tener claro qie existe una repercucion en el flujo de la     de forma asyncrona 
+// tener claro que existe una repercucion en el flujo de la de forma asyncronia 
 
 
 /**
@@ -99,7 +134,7 @@ function crearLectorDeArchivo(nombreArchivo) {
 const LECTOR1 = crearLectorDeArchivo('data.txt');
 LECTOR1.onDataReady(data => {
     console.log(`Primera llamada a data ${data}`);
-    const LECTOR2 = crearLectorDeArchivo('data.txt');
+    const LECTOR2 = crearLectorDeArchivo('data2.txt');
     LECTOR2.onDataReady(data => console.log(`Segunda llamada a ${data}`));
 });
 
@@ -161,7 +196,7 @@ function comprarTresEmpanadasConPromesas(empanadasActuales) {
 function comprarCincoEmpanadasConPromesas(empanadasActuales) {
     const empanadas = empanadasActuales + 5;
     let promesa;
-    return promesa = new Promise((resolve) => resolve(empanadas));
+    return promesa = new Promise(resolve => resolve(empanadas));
 }
 let empanadasConPromesas = 0;
 
@@ -222,9 +257,9 @@ let empanadasAsync = 0;
 async function comprar (){
     const primerCompra = await comprarEmpanadasConAcync(empanadasAsync);
     console.log(`empanadas :  ${primerCompra}`);
-    const segundaCompra = await comprarTresEmpanadasConAcync(empanadasAsync);
+    const segundaCompra = await comprarTresEmpanadasConAcync(primerCompra);
     console.log(`empanadas :  ${segundaCompra}`);
-    const terceraCompra = await comprarCincoEmpanadasConAcync(empanadasAsync);
+    const terceraCompra = await comprarCincoEmpanadasConAcync(segundaCompra);
     console.log(`empanadas :  ${terceraCompra}`);
 }
 
