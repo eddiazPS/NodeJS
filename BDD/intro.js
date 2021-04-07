22abf563d67a87da373f53728b515d6471cded36
eddiazPS <eddiaz@personalsoft.com.co>
2021-04-07 09:41:49 -0500

    intro BDD

--- BDD/intro.js
+++ BDD/intro.js
@@ -10,4 +10,30 @@
  * TDD (codigo directo) BDD (selenum behave) parte de la redaccion 
  * 
  * 
+ * de una espesificacion pueden salir distintos escenarios 
+ * 
+ * Historia de usuario : lengueje humano : 
+ * roll admin gerente vendedor 
+ * :  como actor , 
+ * quiero intencion  : para : motivo beneficio que se intenta lograr  
+ * 
+ * 
+ * ejemplo como usuario quiero publicar mis fotos 
+ * 
+ * quiero registrarme en una pagina para subir fotos 
+ *      escenarios introducir dato incorrecto numero - texto  -> indicar que hay error 
+ *      elegir en una lista desplegable el pais - si no permitir el campo otros 
+ * 
+ * 
+ * dado , cuando y entonces   en nuestras especificaciones 
+ * 
+ * 
+ * crear los escenarios 
+ * 
+ * dado el iMEI cuando lo consulte o cuando lo copare entonces me debe dar este resultado
+ * 
+ * 
+ * dado un request al inviarlo en cierta direccion debo rescibir la respuesta con coigo 200
+ * 
+ * 
  */
